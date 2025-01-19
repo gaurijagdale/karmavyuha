53aad4d82ee5c3765ebdb49053162be32870253c
gaurii134 <gaurijagdalepatil@gmail.com>
2025-01-19 20:35:38 +0530

    small changes

--- frontend/src/pages/manager/ManEmployees.tsx
+++ frontend/src/pages/manager/ManEmployees.tsx
@@ -26,9 +26,9 @@ const ManEmployees = () => {
     <div className="w-full grid grid-cols-2 gap-7 p-10">
       {employees.map((employee) => (
         <Link to={`/empprofile/${employee.user}`} key={employee._id}>
-          <div className="flex space-x-14  bg-gray-900 rounded-xl shadow-md p-4">
+          <div className="flex space-x-14  items-center  bg-gray-900 rounded-xl shadow-md px-14 py-4">
             <img src={profile} alt="" className="rounded-full w-28 h-28" />
-            <div className="flex flex-col">
+            <div className="flex flex-col justify-center ">
               <h3 className="text-white text-xl">{employee.name}</h3>
               <p className="text-gray-400">{employee.department}</p>
               <p className="text-gray-400">{employee.email}</p>
